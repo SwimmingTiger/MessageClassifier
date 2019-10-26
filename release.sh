6017dd744812c53dd55fa91e3a0ec2f30dd685ec
SwimmingTiger <hu60.cn@gmail.com>
2019-10-27 00:34:23 +0800

    add packaging script to repo

--- release.sh
+++ release.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-echo "Download BigWigsMods release.sh"
-curl -o /tmp/BigWigsMods-release.sh https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh
+cd "$(dirname "$0")"
 
 echo "Packaging retail addon"
-bash /tmp/BigWigsMods-release.sh
+bash ./script/BigWigsMods-release.sh
 
 echo "Packaging classic addon"
-bash /tmp/BigWigsMods-release.sh -g 1.13.2
+bash ./script/BigWigsMods-release.sh -g 1.13.2
